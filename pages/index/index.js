591f7c75c685a5637e8ac78deeea0dfb02a1dc54
于海洋 <yuhaiyang0427@gmail.com>
2021-02-04 15:11:53 +0800

    添加搜索框

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,9 +3,20 @@ Page({
     banners: ["https://s1.ax1x.com/2020/10/27/BQUKhQ.jpg", "https://s1.ax1x.com/2020/10/27/BQUutg.jpg",
       "https://s1.ax1x.com/2020/10/27/BQUnAS.jpg", "https://s1.ax1x.com/2020/10/27/BQUe78.jpg"
     ],
-    
+    value:''
   },
-  
+  onCancel:function(){
+    console.log('点击了取消按钮');
+  },
+  onSearch: function(){
+    console.log(this.value);
+    wx.navigateTo({
+      url: '../searchPage/searchPage',
+    })
+  },
+  onFocus: function(){
+    console.log(111);
+  },
   onLoad: function () {
 
   },

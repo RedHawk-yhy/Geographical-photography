1e3f77d3112ae79e8e56ff8b6edbac275bdb92d4
于海洋 <yuhaiyang0427@gmail.com>
2021-02-24 09:49:13 +0800

    网络请求封装

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ Page({
       "https://s3.ax1x.com/2021/02/08/yNjwwD.jpg", "https://s3.ax1x.com/2021/02/08/yNjdeO.jpg"
     ],
     value:'',
-    daily:[{image:'https://s3.ax1x.com/2021/02/08/yNjDFH.jpg',content:'我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字'},{image:'https://s3.ax1x.com/2021/02/08/yNj0Te.jpg',content:'我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字'},{image:'https://s3.ax1x.com/2021/02/08/yNjwwD.jpg',content:'我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字'},{image:'https://s3.ax1x.com/2021/02/08/yNjdeO.jpg',content:'我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字'}]
+    daily:[{image:'https://s3.ax1x.com/2021/02/08/yNjDFH.jpg',content:'我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字'},{image:'https://s3.ax1x.com/2021/02/08/yNj0Te.jpg',content:'我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字'},{image:'https://s3.ax1x.com/2021/02/08/yNjwwD.jpg',content:'我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字'},{image:'https://s3.ax1x.com/2021/02/08/yNjdeO.jpg',content:'我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字我是文字'}]
   },
   onCancel:function(){
     console.log('点击了取消按钮');
